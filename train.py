ddba30a4628e819c2a696ded97da05b8f8291a48
Nandakishor <nandumpilicode@gmail.com>
2025-12-12 21:24:03 +0530

    ss

--- train.py
+++ train.py
@@ -78,8 +78,7 @@ class Trainer:
             self.optimizer,
             mode='min',
             factor=0.5,
-            patience=5,
-            verbose=True
+            patience=5
         )
         
         # Early stopping
